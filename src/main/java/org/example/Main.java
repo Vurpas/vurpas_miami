50445aa7d0ecdd492955140241cc5ca4e0cab95b
Vurpas <erikengstrom164@gmail.com>
2024-02-14 10:50:51 +0100

    lol

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,13 @@ public class Main {
         int x = random.nextInt(5);
         System.out.println("THE ROLL OF THE DICE IS: " + x);
         if(x == 0) {
-            System.out.println("YOU WILL WORK WITH 'create user CRUD operations'");
+            System.out.println("Hello world'");
         } else if (x == 1) {
             System.out.println("YOU WILL WORK WITH 'create gameAd CRUD operations'");
         } else if (x == 2) {
             System.out.println("YOU WILL WORK WITH 'create order CRUD operations'");
         } else if (x == 3) {
-            System.out.println("YOU WILL WORK WITH 'SECURITY'");
+            System.out.println("Trolololo!");
         } else if (x == 4) {
             System.out.println("YOU WILL WORK WITH 'SECURITY'");
         }
