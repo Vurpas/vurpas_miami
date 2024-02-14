070edf9292d63188f738676588c68014d2da3bcd
Vurpas <erikengstrom164@gmail.com>
2024-02-14 09:25:14 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int x = random.nextInt(5);
+        System.out.println("THE ROLL OF THE DICE IS: " + x);
+        if(x == 0) {
+            System.out.println("YOU WILL WORK WITH 'create user CRUD operations'");
+        } else if (x == 1) {
+            System.out.println("YOU WILL WORK WITH 'create gameAd CRUD operations'");
+        } else if (x == 2) {
+            System.out.println("YOU WILL WORK WITH 'create order CRUD operations'");
+        } else if (x == 3) {
+            System.out.println("YOU WILL WORK WITH 'SECURITY'");
+        } else if (x == 4) {
+            System.out.println("YOU WILL WORK WITH 'SECURITY'");
+        }
+    }
+}
